2f2111487620fe5ed9218bfb73abeecf31041e70
Vlad <goncharov-vlad@ukr.net>
2019-01-15 15:45:36 +0200

    initial commit

--- dist/script.js
+++ dist/script.js
@@ -1,5 +1,9 @@
 (($) => {
     $(document).ready((e) => {
-
-    })
+        $('.parent-container').magnificPopup({
+            delegate: 'a', // child items selector, by clicking on it popup will open
+            type: 'image'
+            // other options
+        });
+    });
 }) (jQuery);
