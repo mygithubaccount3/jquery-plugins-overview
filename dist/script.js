ff763e7db4db1600a7ff9998db7f58cd14132993
Vlad <goncharov-vlad@ukr.net>
2019-01-19 14:49:21 +0200

    initial commit

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,23 @@
 (($) => {
     $(document).ready((e) => {
-        $('.parent-container').magnificPopup({
+
+        const $navigation = $('<nav class="navigation"></nav>');
+        const $plugins = $('.plugin');
+        $plugins.each((element) => {
+            const $this = $($plugins[element]);
+            const $href = ${$this.attr('id');
+            const $name = ${$this.find('.plugin__title').text();
+            $navigation.append($(`<a class="navigation__item" href="#${$href}">${$this.find('.plugin__title').text()}</a>`));
+        });
+        $navigation.prependTo($('body'));
+
+        $('.js-magnific-popup').magnificPopup({
             delegate: 'a', // child items selector, by clicking on it popup will open
             type: 'image'
             // other options
+        });
+        $('.popup-images').magnificPopup({
+            type: 'image'
         });
     });
 }) (jQuery);
